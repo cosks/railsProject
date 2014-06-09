8366ba89f0c777caf0f1063c42039b6bc9f6c089
kkorenev <kkoernev@hotmail.com>
2014-06-09 15:54:03 +0100

    hope this works

---
+++ Projects/test/test/Dialog.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace test
+{
+	public partial class Dialog : Gtk.Dialog
+	{
+		public Dialog ()
+		{
+			this.Build ();
+		}
+	}
+}
+
---
+++ Projects/test/test/gtk-gui/test.Window.cs
@@ -0,0 +1,7 @@
+
+namespace test
+{
+	public partial class Window
+	{
+	}
+}
